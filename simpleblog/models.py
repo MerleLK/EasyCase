ceeff1744dbdfc8c2c39cbd10fe232e5cafc3437
MerleLiukun <liukun@bestminr.com>
2017-10-29 22:21:53 +0800

    modify the model

--- simpleblog/models.py
+++ simpleblog/models.py
@@ -2,6 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class BaseModel(models.Model):
+    """
+    the basic custom's model
+    """
+    created_at = models.DateTimeField(
+        verbose_name='创建时间',
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name='更新时间',
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = [
+            'created_at',
+            'author',
+        ]
+
+
 class Category(models.Model):
     name = models.CharField(
         verbose_name='文章分类',
@@ -16,7 +37,7 @@ class Tag(models.Model):
     )
 
 
-class Post(models.Model):
+class Post(BaseModel):
 
     title = models.CharField(
         verbose_name='文章标题',
@@ -42,13 +63,4 @@ class Post(models.Model):
 
     author = models.ForeignKey(
         User,
-    )
-
-    created_at = models.DateTimeField(
-        verbose_name='创建时间',
-        auto_now_add=True,
-    )
-    updated_at = models.DateTimeField(
-        verbose_name='更新时间',
-        auto_now=True,
     )
